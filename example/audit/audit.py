20825c4f6d3f84fb4049242aff99301a09e60732
asavershin <avershinrussian@gmail.com>
2024-08-26 13:03:38 +0300

    Add new param for Audit

--- example/audit/audit.py
+++ example/audit/audit.py
@@ -1,8 +1,10 @@
 class Audit:
-    def __init__(self, audit_topic):
+    def __init__(self, system, audit_topic):
         self._audit_topic = audit_topic
+        self._system_name = system
 
     def __call__(self, method):
         method._is_audit = True
         method.audit_topic = self._audit_topic
+        method.system_name = self._system_name
         return method
